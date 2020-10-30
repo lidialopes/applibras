cf0efb0945fcd1e3795330c3ad4769c7cb5cf1bc
lidialopes <lidiajlopes@gmail.com>
2020-10-30 17:35:20 -0300

    add data to About

--- applibras/src/pages/About/styles.ts
+++ applibras/src/pages/About/styles.ts
@@ -1,21 +1,7 @@
 import styled from 'styled-components/native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 import { FlatList } from 'react-native';
 import { Info } from './index';
-
-export const Image = styled.View`
-    border-radius: 100px;
-    border-width: 1px;
-    border-color: #aaaaaa;
-    height: 80px;
-    width: 80px;
-    margin: 0px 10px 0px 0px ;
-`;
-
-export const Name = styled.Text`
-    font-family: 'OpenSans-SemiBold';
-    font-size: 15px;
-    color: #212121;
-`;
 
 export const DescriptionProject = styled.Text`
     font-family: OpenSans;
@@ -26,30 +12,64 @@ export const DescriptionProject = styled.Text`
     margin-top: 10px;
 `;
 
-export const Description = styled.Text`
-    font-family: OpenSans;
-    font-size: 15px;
-    color: #212121;
-    text-align: justify;
-    letter-spacing: 0.25px;
-`;
-
-export const InfoList = styled(FlatList as new () => FlatList<Info>)`
+export const InfoList = styled.ScrollView`
     margin: 15px 0 15px 0;
+    padding: 0 10px 0 0;
 `;
 
 export const InfoContainer = styled.View`
     flex-direction: row;
     align-items: center;
-    justify-content: flex-start;
+    justify-content: center;
 
-    margin: 0px 0px 10px 0px;
+    margin: 0px 5px 10px 0px;
     padding-bottom: 10px;
     border-bottom-color: #dbdbdb;
     border-bottom-width: 0.75px; 
 `;
-    
+
+export const Image = styled.Image`
+    border-radius: 100px;
+    border-width: 1px;
+    border-color: #aaaaaa;
+    height: 90px;
+    width: 90px;
+    margin: 0px 10px 0px 0px ;
+`;
+
 export const TextInfo = styled.View`
     flex: 1;
     flex-direction: column;
+    justify-content: space-around;
+`;
+
+export const Name = styled.Text`
+    font-family: 'OpenSans-SemiBold';
+    font-size: 15px;
+    color: #212121;
+`;
+
+export const Description = styled.Text`
+    font-family: OpenSans;
+    font-size: 15px;
+    color: #212121;
+    text-align: justify;
+`;    
+
+export const Contact = styled.View`
+    flex-direction: row;
+    align-items: flex-start;
+    margin-top: 5px;
+`;
+
+export const ContactText = styled.Text`
+    font-family: OpenSans;
+    font-size: 15px;
+    color: #bdbdbd;
+    text-align: justify;
+    flex: 1;
+`;
+
+export const Icon = styled(FeatherIcon)`
+  margin-right: 5px;
 `;
