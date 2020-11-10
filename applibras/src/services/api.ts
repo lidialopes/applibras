de2f64ee6b64aac0638dcb980a7129d23328f598
lidialopes <lidiajlopes@gmail.com>
2020-11-10 20:28:59 -0300

    change baseURL

--- applibras/src/services/api.ts
+++ applibras/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http:/192.168.1.8:3333/'
+    baseURL: 'https://manuarioinformatica.herokuapp.com/'
 });
 
 export default api;
