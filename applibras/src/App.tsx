04fa40369d3288348c03ed2225a853cb3f645eda
lidialopes <lidiajlopes@gmail.com>
2020-11-11 20:36:31 -0300

    set splash screen

--- applibras/src/App.tsx
+++ applibras/src/App.tsx
@@ -1,18 +1,26 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import RNBootSplash from "react-native-bootsplash";
 
 import Routes from './routes';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#007BF9" />
-    <View style={{ backgroundColor: '#fafafa', flex: 1 }}>
-      <Routes />
-    </View>
-  </NavigationContainer>
-);
+const App: React.FC = () => { 
+
+  useEffect(() => (
+      RNBootSplash.hide({ duration: 250 })
+  ), []);
+
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#007BF9" />
+      <View style={{ backgroundColor: '#fafafa', flex: 1 }}>
+        <Routes />
+      </View>
+    </NavigationContainer>
+  );
+}
 
 export default App;
